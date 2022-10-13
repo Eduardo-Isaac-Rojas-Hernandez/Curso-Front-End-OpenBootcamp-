d9d76e556b73db057a26608b7ca9dcb1109d2f8b
Eduardo-Isaac-Rojas-Hernandez <114044808+Eduardo-Isaac-Rojas-Hernandez@users.noreply.github.com>
2022-10-13 18:44:31 +0200

    Add files via upload

---
+++ Javascript-básico/02-Sintaxis-variables/index.js
@@ -0,0 +1,16 @@
+
+cons lista1=[
+    "Eduardo Isaac Rojas Hernandez", 
+    33, 
+    true, 
+    new Date(1988, 14, 11), 
+    
+    {
+        titulo:"Alicia en el pais de las maravillas",
+        autor:"Lewis Carrol",
+        fecha:1865,
+        url:"https://www.amazon.com.mx/ALICIA-EN-PA%C3%8DS-LAS-MARAVILLAS/dp/6070749111"
+    }
+]
+
+
