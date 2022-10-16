87c5113d712649cb632b03e8b3ac058e0b3d0ed7
Eduardo-Isaac-Rojas-Hernandez <114044808+Eduardo-Isaac-Rojas-Hernandez@users.noreply.github.com>
2022-10-16 04:14:09 +0200

    Add files via upload

---
+++ Javascript-básico/05-Numbers/numeros.js
@@ -0,0 +1,20 @@
+const altura_cm = 185;
+
+const altura_m = 1.85;
+
+const peso_kg = 82.54;
+
+const redondear_arriba = Math.round(altura_m);
+
+const redondear_abajo = Math.floor(peso_kg);
+
+var max_valor;
+
+if(Number.MAX_VALUE+1 === Number.MAX_VALUE) {
+    max_valor = true;
+}else {
+    max_valor = false;
+}
+
+
+
