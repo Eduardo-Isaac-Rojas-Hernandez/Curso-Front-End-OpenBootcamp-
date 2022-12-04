0e0b3540cbabd3237115075f8259cf28e101800b
Eduardo-Isaac-Rojas-Hernandez <114044808+Eduardo-Isaac-Rojas-Hernandez@users.noreply.github.com>
2022-12-03 22:11:41 -0600

    Add files via upload

---
+++ Javascript-básico/07-Sets_Objetos/fechas.js
@@ -0,0 +1,13 @@
+const fecha = new Date()
+
+const fecha2 = new Date(1988,10,14)
+
+const comparaFechas = new Date() > fecha2
+
+const miNacimiento = fecha2
+
+const diaNacimiento = miNacimiento.getDate()
+
+const mesNacimiento = miNacimiento.getMonth()
+
+const anioNacimiento = miNacimiento.getFullYear()
